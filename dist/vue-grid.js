439ea2b14f580eddd38e87b6ff82a4d8182db0a1
wrainCN <123723620@qq.com>
2016-09-01 23:35:08 +0800

    第一次提交！

---
+++ dist/vue-grid.js
@@ -0,0 +1,19 @@
+/*
+* Design by wrainCN 2016-09-01
+*/
+vueGrid.install = function (Vue, options) {
+    // 1. 添加全局方法或属性
+    Vue.vueGridMethod = {
+
+    }
+
+    // 2. 添加全局资源
+    Vue.directive('vue-grid', {
+
+    })
+
+    // 3. 添加实例方法
+    Vue.prototype.$vueGrid = {
+
+    }
+}
